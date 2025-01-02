e8711b41065efd99ca917388d95a9e90435ec5e0
Khurrum <khurrum1@gmail.com>
2025-01-02 10:00:48 -0500

    Updates

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../src/styles/global';
+import theme from '../src/styles/theme';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
